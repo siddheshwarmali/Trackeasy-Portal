0216bb14cc94b1174379fe4ad72074179584c19c
siddheshwarmali <65440016+siddheshwarmali@users.noreply.github.com>
2026-01-11 14:18:39 +0530

    Update me.js

--- api/auth/me.js
+++ api/auth/me.js
@@ -1,6 +1,5 @@
 // api/auth/me.js
 const { getAuth, json } = require('../_lib/auth');
-
 module.exports = async (req, res) => {
   const auth = getAuth(req);
   if (!auth) return json(res, 200, { authenticated: false });
