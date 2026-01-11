59488ca4fbc1caf0ea4308109c635885c0898a7b
siddheshwarmali <65440016+siddheshwarmali@users.noreply.github.com>
2026-01-11 14:57:23 +0530

    ok

---
+++ logout.js
@@ -0,0 +1,6 @@
+// api/auth/logout.js
+const { clearAuthCookie, json } = require('../_lib/auth');
+module.exports = async (req, res) => {
+  clearAuthCookie(res);
+  return json(res, 200, { ok: true });
+};
