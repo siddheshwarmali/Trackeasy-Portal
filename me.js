fc8883080fbcc82b7ac43612a6676a64d91dd3ff
siddheshwarmali <65440016+siddheshwarmali@users.noreply.github.com>
2026-01-11 14:58:18 +0530

    Delete me.js

--- me.js
+++
@@ -1,7 +0,0 @@
-// api/auth/me.js
-const { getAuth, json } = require('../_lib/auth');
-module.exports = async (req, res) => {
-  const auth = getAuth(req);
-  if (!auth) return json(res, 200, { authenticated: false });
-  return json(res, 200, { authenticated: true, ...auth });
-};
