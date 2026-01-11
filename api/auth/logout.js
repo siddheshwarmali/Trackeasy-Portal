d8335043e6b5ae1ea6df4c45426cb7d157e5b4c7
siddheshwarmali <65440016+siddheshwarmali@users.noreply.github.com>
2026-01-11 14:18:10 +0530

    Update logout.js

--- api/auth/logout.js
+++ api/auth/logout.js
@@ -1,6 +1,5 @@
 // api/auth/logout.js
 const { clearAuthCookie, json } = require('../_lib/auth');
-
 module.exports = async (req, res) => {
   clearAuthCookie(res);
   return json(res, 200, { ok: true });
