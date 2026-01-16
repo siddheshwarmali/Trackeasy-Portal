9c1367e67da83261aea42374f5ed1679451af15b
siddheshwarmali <65440016+siddheshwarmali@users.noreply.github.com>
2026-01-16 22:23:25 +0530

    Delete api directory

--- api/auth/logout.js
+++
@@ -1,8 +0,0 @@
-const { clearCookie } = require('../../lib/auth');
-const { sendJson } = require('../../lib/http');
-
-module.exports = async (req, res) => {
-  if (req.method !== 'POST') return sendJson(res, 405, { error: 'Method not allowed' });
-  clearCookie(res);
-  return sendJson(res, 200, { ok: true });
-};
