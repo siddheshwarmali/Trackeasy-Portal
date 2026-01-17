615f9e4e4f1b7fd9d9d0a485bc94a09ec25ee49e
siddheshwarmali <65440016+siddheshwarmali@users.noreply.github.com>
2026-01-17 13:21:23 +0530

    up

--- api/ping.js
+++ api/ping.js
@@ -1,14 +1,1 @@
-'use strict';
-module.exports = async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type','application/json');
-  res.end(JSON.stringify({
-    ok: true,
-    env: {
-      GITHUB_TOKEN: !!process.env.GITHUB_TOKEN,
-      GITHUB_OWNER: !!process.env.GITHUB_OWNER,
-      GITHUB_REPO: !!process.env.GITHUB_REPO,
-      GITHUB_USERS_FILE: !!process.env.GITHUB_USERS_FILE
-    }
-  }));
-};
+module.exports=(req,res)=>res.json({ok:true});
